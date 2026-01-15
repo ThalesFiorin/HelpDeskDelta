32ea37d7062fa56036552af0ec0209cec4a94161
ThalesFiorin <thalesfiorin@gmail.com>
2026-01-15 18:50:21 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'class', 
+module.exports = {
+  darkMode: 'class', // Isso impede o celular de mudar as cores sozinho
   content: [
-    "./index.html",
-    "./*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './index.html',
+    './App.tsx',
+    './index.tsx',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
         delta: {
-          yellow: '#F8AB1B',
-          blue: '#426FA6',
-          dark: '#4D4D4D',
-        }
-      }
+          yellow: '#F8AB1B', // O dourado da sua logo
+          blue: '#426FA6',   // O azul exato da sua barra lateral
+          dark: '#4D4D4D',   // O cinza escuro dos seus textos
+        },
+      },
     },
   },
   plugins: [],
